c42c72e2faeff759139f6ee69928a5dab6ce9b95
MaxRem <maxzheng0302@gmail.com>
2025-06-19 21:30:55 +0800

    Add features for svm testing

--- max_space/GNNparser.cpp
+++ max_space/GNNparser.cpp
@@ -6,29 +6,14 @@
 #include <string>
 #include <algorithm>
 #include <regex>
+#include <climits>
 
 using namespace std;
 
 // type to one hot encoding
 unordered_map<string, int> type_to_index;
 int num_types;
-
-void initialize(){
-    // Primitive gates (and, or, nand, nor, not, buf, xor, xnor) and dff
-    type_to_index["input"] = 0;
-    type_to_index["output"] = 1;
-    type_to_index["and"] = 2;
-    type_to_index["or"] = 3;
-    type_to_index["nand"] = 4;
-    type_to_index["nor"] = 5;
-    type_to_index["not"] = 6;
-    type_to_index["buf"] = 7;
-    type_to_index["xor"] = 8;
-    type_to_index["xnor"] = 9;
-    type_to_index["dff"] = 10;
-    num_types = type_to_index.size();
-}
-
+int num_features;
 /*
 for (const auto& [name, g] : nodes) {
     if (type_to_index.count(g.type) == 0) {
@@ -101,24 +86,183 @@ struct Node {
     vector<string> inputs;
 };
 
+// global maps and vectors
+unordered_map<string, int> FFI;
+unordered_map<string, int> FFO;
+unordered_map<string, int> PI;
+unordered_map<string, int> PO;
+unordered_map<string, int> LGFI;
+
+unordered_map<string, Node> nodes;                   // node_name -> Node
+unordered_map<string, string> wire_to_node;          // wire_name -> node_name
+unordered_map<string, int> node_id_map;              // node_name -> id
+unordered_map<string, string> dff_dq_map;           // d <-> q
+
+vector<string> primary_input_node_name;
+vector<string> primary_output_node_name;
+vector<string> dff_node_name;
+vector<string> dff_input_name;
+
+vector<pair<int, string>> sorted_nodes; // (id, name)
+
+int lgfi(string index){
+    if(nodes[index].inputs.size() <= 0){
+        return 1;
+    }
+    else {
+        int temp = 0;
+        for(const auto& ancestor:nodes[index].inputs){
+            //cout << ancestor << ":" << nodes[ancestor].inputs.size() << endl;
+            temp += (nodes[wire_to_node[ancestor]].inputs.size()<=0)?1:nodes[wire_to_node[ancestor]].inputs.size();
+        }
+        return temp;
+    }
+}
+
+int pi(string index){
+    if(PI.count(index) > 0){
+        return PI[index];
+    }
+    else if(nodes[index].type == "input"){
+        PI[index] = 0;
+        return PI[index];
+    }
+    else if(nodes[index].type == "dff"){
+        PI[index] = INT_MAX; // fear of cycle
+        return PI[index];
+    }
+    else{
+       if(nodes[index].inputs.size() <= 0){
+            PI[index] = INT_MAX;
+            cout << "Connect to a constant" << endl;
+            return PI[index];
+        }
+        else{
+            int temp = INT_MAX;
+            for(const auto& ancestor : nodes[index].inputs){
+                if(pi(wire_to_node[ancestor]) < temp) temp = PI[wire_to_node[ancestor]];
+            }
+            PI[index] = (temp == INT_MAX)? INT_MAX : (temp+1);
+            return PI[index];
+        } 
+    }
+}
+
+int po(){}
+
+int ffi(string index){}
+
+int ffo(string index){
+    if(FFO.count(index) > 0){
+        return FFO[index];
+    }
+    else if(nodes[index].type == "dff"){
+        // itself is a dff output
+        FFO[index] = 0;
+        return FFO[index];
+    }
+    else {
+        if(nodes[index].inputs.size() <= 0){
+            FFO[index] = INT_MAX;
+            return FFO[index];
+        }
+        else{
+            int temp = INT_MAX;
+            for(const auto& ancestor : nodes[index].inputs){
+                if(ffo(wire_to_node[ancestor]) < temp) temp = FFO[wire_to_node[ancestor]];
+            }
+            FFO[index] = (temp == INT_MAX)? INT_MAX : (temp+1);
+            return FFO[index];
+        }
+    }
+}
+
+void FeatureExtraction(){
+    // calculate lgfi
+    cout << "=========================LGFI=========================" << endl;
+    for (const auto& [id, name] : sorted_nodes){
+        LGFI[name] = lgfi(name);
+        cout << name << " => " << LGFI[name] << endl;
+    }
+    // calculate pi and ffo
+    cout << "Start recursive part" << endl;
+    int dummy;
+    for (const auto& output_port: primary_output_node_name){
+        dummy = pi(output_port);
+        dummy = ffo(output_port);
+    }
+    for (const auto& ff_input: dff_input_name){
+        if(valid(ff_input)){
+            dummy = pi(wire_to_node[ff_input]);
+            dummy = ffo(wire_to_node[ff_input]);
+        }
+    }
+    for (const auto& ff_output: dff_node_name){
+        PI[ff_output] = 1 + PI[wire_to_node[dff_dq_map[ff_output]]];
+        FFO[ff_output] = 1 + FFO[wire_to_node[dff_dq_map[ff_output]]];
+    }
+    cout << "=========================PI=========================" << endl;
+    for (const auto& [key, value] : PI) {
+        cout << key << " => " << value << endl;
+    }
+    cout << "=========================FFO=========================" << endl;
+    for (const auto& [key, value] : FFO) {
+        cout << key << " => " << value << endl;
+    }
+}
+
+void initialize(){
+    // Primitive gates (and, or, nand, nor, not, buf, xor, xnor) and dff
+    type_to_index["input"] = 0;
+    type_to_index["output"] = 1;
+    type_to_index["and"] = 2;
+    type_to_index["or"] = 3;
+    type_to_index["nand"] = 4;
+    type_to_index["nor"] = 5;
+    type_to_index["not"] = 6;
+    type_to_index["buf"] = 7;
+    type_to_index["xor"] = 8;
+    type_to_index["xnor"] = 9;
+    type_to_index["dff"] = 10;
+    num_types = type_to_index.size();
+    num_features = 3;
+}
+
+void start_and_clear(){
+    FFI.clear();
+    FFO.clear();
+    PI.clear();
+    PO.clear();
+    LGFI.clear();
+    nodes.clear();
+    wire_to_node.clear();
+    node_id_map.clear();
+    primary_input_node_name.clear();
+    primary_output_node_name.clear();
+    dff_node_name.clear();
+    dff_input_name.clear();
+    sorted_nodes.clear();
+    dff_dq_map.clear();
+}
+
+
 int main(){
 
-    //ifstream infile("../release(20250517)/release/design9.v");
-    ifstream infile("circuit.v");
+    ifstream infile("../release(20250517)/release/design0.v");
+    //ifstream infile("circuit.v");
     ofstream edgefile("edges.csv");
     ofstream nodefile("nodetypes.csv");
     ofstream GNNedgefile("GNNedges.csv");
     ofstream GNNnodefile("GNNnodetypes.csv");
-
-    unordered_map<string, Node> nodes;                   // node_name -> Node
-    unordered_map<string, string> wire_to_node;          // wire_name -> node_name
-    unordered_map<string, int> node_id_map;              // node_name -> id
+    ofstream GNNfeaturefile("GNNfeature.csv");
 
     initialize();
 
     string line;
     int node_id_counter = 0;
     int line_count = 0;
+
+    start_and_clear();
 
     while(getline(infile, line)){
 
@@ -180,11 +324,14 @@ int main(){
             dff.name = node_name;
             dff.type = type;
             if(valid(tokens[4])) dff.output = tokens[4];
+            dff_dq_map[node_name] = tokens[3];
             dff.inputs = vector<string>(tokens.begin(), tokens.end()-1);
+            dff_input_name.insert(dff_input_name.end(), tokens.begin(), tokens.end()-1);
             // three hash tables!
             nodes[node_name] = dff;
             if(!dff.output.empty()) wire_to_node[dff.output] = node_name; // therefore, the node is equivalent to output
             node_id_map[node_name] = node_id_counter++;
+            dff_node_name.push_back(node_name);
         }
         else if(type == "input"){
             string rest;
@@ -199,6 +346,7 @@ int main(){
                 nodes[port_name] = p;
                 wire_to_node[p.output] = port_name;
                 node_id_map[port_name] = node_id_counter++;
+                primary_input_node_name.push_back(port_name);
             }
         }
         else if(type == "output"){
@@ -213,6 +361,7 @@ int main(){
                 p.inputs.push_back(port_name);
                 nodes[port_name] = p;
                 node_id_map[port_name] = node_id_counter++;
+                primary_output_node_name.push_back(port_name);
             }
         }
         else{
@@ -220,7 +369,6 @@ int main(){
         }
     }
 
-    vector<pair<int, string>> sorted_nodes; // (id, name)
     for (const auto& [name, id] : node_id_map) {
         sorted_nodes.emplace_back(id, name);
     }
@@ -278,6 +426,27 @@ int main(){
                 }
             }
         }
+    }
+
+    /*
+    unordered_map<string, Node> nodes;                   // node_name -> Node
+    unordered_map<string, string> wire_to_node;          // wire_name -> node_name
+    unordered_map<string, int> node_id_map;              // node_name -> id
+    
+    vector<string> primary_input_node_name;
+    vector<string> primary_output_node_name;
+    vector<string> dff_node_name;
+    */
+
+    FeatureExtraction();
+    // header
+    GNNfeaturefile << "id,name";
+    for (int i = 0; i < num_features; ++i) {
+        GNNfeaturefile << ",feature_" << i;
+    }
+    GNNfeaturefile << endl;
+    for (const auto& [id, name] : sorted_nodes) {
+        GNNfeaturefile << id << "," << name << "," << PI[name] << "," << FFO[name] << "," << LGFI[name] << endl;
     }
 
     return 0;
